ef7a9a4d98a52b017391092fde50838fa52e879b
Odoo <odoo@aktivsoftware.com>
2017-11-25 18:35:32 +0530

    Initial commit for hr skill qualification in 10C for master branch.

---
+++ hr_skill_qualification/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name':'Employee Skills and Qualifications',
+    'category':'Human Resources',
+    'version':'1.1',
+    'website': 'http://www.aktivsoftware.com',
+    'author': 'Aktiv Software',
+    'license': 'AGPL-3',
+    'description':'Manage Employee Skill and Qualifications.',
+    
+    'depends':[
+       'hr_recruitment'
+        ],
+    
+    'data':[
+        'security/ir.model.access.csv',
+        'views/hr_employee_skill_qualification_views.xml',
+        'views/hr_applicant_skill_qualification.xml'
+        ],
+    
+    'auto_install':False,
+    'installable':True,
+    'application':False    
+    
+    }
